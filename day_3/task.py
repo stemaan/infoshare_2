2cda829580c1c17b08be72874c9f5aa2207b31cb
Przemysław Lalak <steman@protonmail.com>
2018-02-05 20:27:05 +0100

    do dokoczenia sprawdzanie czy liczba jest parzysta

---
+++ day_3/task.py
@@ -0,0 +1,8 @@
+digit = input('Podaj liczbe: ')
+# zamiana str na int
+digit = int(digit)
+
+#@ TODO:
+if is_even:
+    print('licza jest parzysta')
+#@ TODO: w przeciwnym wypadku wyswietl tekst 'liczba nie jest parzysta'

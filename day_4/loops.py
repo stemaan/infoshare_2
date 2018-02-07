fea67e42149b0e80d65c4be68954adc36ebcd480
Przemysław Lalak <steman@protonmail.com>
2018-02-07 18:17:51 +0100

    while loop example

---
+++ day_4/loops.py
@@ -0,0 +1,14 @@
+# petla nieskonczona
+# while True:
+#     print('Hello')
+
+counter = 0
+value = input('Podaj liczbe: ')
+value = int(value)
+
+while counter < value:
+    # ten kod wykonuje sie w petli
+    print('hello')
+    counter += 1
+# ten juz poza petla
+print('bye')

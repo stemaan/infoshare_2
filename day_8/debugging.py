07a742ba3ea228a688c62b787e5ec581a256a5d3
Przemysław Lalak <steman@protonmail.com>
2018-02-28 19:31:50 +0100

    debugging with exceptions

--- day_8/debugging.py
+++ day_8/debugging.py
@@ -1,8 +1,6 @@
-import pdb
-pdb.set_trace()
 import os
 # dane
-monety =        [5, 2, 1, 0.5, 0.2, 0.1]
+monety =        [0, 2, 1, 0.5, 0.2, 0.1]
 monety_reszta = [0, 0, 0,   0,   0,   0]
 
 banknot = 20
@@ -14,9 +12,12 @@ indeks_mon_reszta = 0
 
 for moneta in monety:
     if reszta >= moneta:
-        ilosc = reszta // moneta
-        wartosc = ilosc * moneta
-        reszta = reszta - wartosc
+        try:
+            ilosc = reszta // moneta
+            wartosc = ilosc * moneta
+            reszta = reszta - wartosc
+        except (ZeroDivisionError, NameError) as error:
+            continue
         os.path.split('/home/')
 
         monety_reszta[indeks_mon_reszta] = ilosc
@@ -24,3 +25,4 @@ for moneta in monety:
     indeks_mon_reszta += 1
 
 print("Reszta do wydania:")
+

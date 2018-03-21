fe715c8f8ce65a4dd5f43f14fe93e4794468529c
przemek <s>
2018-03-22 00:23:34 +0100

    add Phonebook class

--- day_14/library.py
+++ day_14/library.py
@@ -19,8 +19,40 @@ class Contact:
         return f'Contact({self.name}, {self.lastname})'
 
     def __del__(self):
-        print('called')
+        print('called', self.id)
         Contact.contacts_no -= 1
+
+
+class Phonebook:
+    def __init__(self):
+        self.__data = dict()
+
+    def __len__(self):
+        return len(self.__data)
+
+    def __str__(self):
+        elements = [f'{k} -> {v}' for k, v in self.__data.items()]
+        elements_with_new_lines = '\n'.join(elements)
+        return elements_with_new_lines
+
+    def __iter__(self):
+        pass
+
+    def __getitem__(self, item):
+        return self.__data[item]
+
+    def __delitem__(self, key):
+        return self.__data.pop(key)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def add_contact(self, contact):
+        self.__data[contact.id] = contact
+
+    def remove_contact(self, contact):
+        self.__delitem__(contact.id)
+
 
 
 if __name__ == '__main__':
@@ -31,8 +63,22 @@ if __name__ == '__main__':
     c2 = Contact('Adam', 'Nowak')
     print(c)
     print(c2)
-    contacts = [c, c2]
+    contacts = [c, c2, Contact('Pawel', 'Nowak')]
     pprint(contacts)
+    del contacts[1]
     del contacts[1]
     pprint(contacts)
     print(Contact.contacts_no)
+
+    phonebook = Phonebook()
+    phonebook.add_contact(c)
+    phonebook.add_contact(c2)
+    phonebook.add_contact(Contact('Pawel', 'Nowak'))
+    # print(phonebook)
+    # print(phonebook[1])
+    # print(phonebook[2])
+    # print('*'*80)
+    phonebook.remove_contact(c)
+
+    # for contact in phonebook:
+    #     print(contact)
